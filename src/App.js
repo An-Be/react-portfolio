95a8296bc197f7ecab48f32e12b01d14fc4ff360
Andrea <andrea.berrocal95@gmail.com>
2022-08-19 21:07:45 -0700

    added routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Header from './components/Header';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
