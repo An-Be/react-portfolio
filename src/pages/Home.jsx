abab968a2971163839aadf671162ce661bf7fb6d
Andrea <andrea.berrocal95@gmail.com>
2022-10-08 20:43:03 -0700

    added grid to fix layout of app, added starter code for project section, changed colors of app, and changed pfp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
-import pfp from '../images/pfp.png';
+import pfp from '../images/pfp.jpg';
 import { FaNodeJs, FaReact, FaJava, FaHtml5, FaCss3Alt, FaPython } from 'react-icons/fa'
-import { SiJavascript } from 'react-icons/si'
+import { SiJavascript, SiSplunk } from 'react-icons/si'
 import { TbCSharp } from 'react-icons/tb';
 import { AiOutlineConsoleSql } from 'react-icons/ai'
 
@@ -9,14 +9,12 @@ const Home = () => {
     const iconSize = 40;
     
     return(
-        <section>
-            <div>
-                <h1>Hello my name is</h1>
-                <p>Andrea Berrocal</p>
-                <img src={pfp} alt="andrea bitmoji" />
-                <p>I have a knack for building cool things for the web</p>
-                <p>I'm a software engineer currently focused in react.js. I have an interest in game development and Artificial Intelligence.</p>
-                <div>
+        <section className='home'>
+            <div className='home-container'>
+                <div className='personal-info'>
+                    <h1>Hello my name is <span>Andrea Berrocal</span></h1>
+                    <p>I have a knack for building cool things for the web. I'm a software engineer currently focused in react.js. I have an interest in game development and Artificial Intelligence. I currently work at American Express.</p>
+                    <div className='technologies'>
                     <a href="//"><FaReact size={iconSize}/></a>
                     <a href="//"><SiJavascript size={iconSize} /></a>
                     <a href="//"><FaHtml5 size={iconSize} /></a>
@@ -26,7 +24,14 @@ const Home = () => {
                     <a href="//"><FaPython size={iconSize} /></a>
                     <a href="//"><FaJava size={iconSize} /></a>
                     <a href="//"><TbCSharp size={iconSize} /></a>
+                    <a href="//"><SiSplunk size={iconSize} /></a>
+
                 </div>
+                </div>
+                <div>
+                <img src={pfp} alt="andrea bitmoji" width={200} />
+                </div>
+                
             </div>
 
         </section>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,76 @@
 const Projects = () => {
 
-    return <div></div>
+    const projects = [
+        {
+            name: 'Password Generator',
+            description: 'Built with vanilla Javascript, HTML, and CSS.',
+            repo: 'https://github.com/An-Be/password-generator',
+            deployed: true,
+            live: 'https://password-generator-an-be.vercel.app/'
+
+        },
+        {
+            name: 'Top Anime Archive',
+            description: 'A very scaled down version of My Anime list. Built using React, Firebase, and Jikan API',
+            repo: 'https://github.com/An-Be/top-anime-archive',
+            deployed: false,
+            live: ''
+
+        },
+        {
+            name: 'Blog API',
+            description: 'Backend for a Blog. Built using Express and Sequelize',
+            repo: 'https://github.com/An-Be/BLOG_API',
+            deployed: false,
+            live: ''
+
+        },
+        {
+            name: 'Non traditionals Blog',
+            description: 'A blog built with built with ASP.Net Core. Using MVC model and Entity Framework.',
+            repo: 'https://github.com/An-Be/NonTraditionalsBlog',
+            deployed: false,
+            live: ''
+
+        },
+        {
+            name: 'Moviefy',
+            description: 'ASP.NET Core MVC App that uses CRUD operations and policy-based authentication.',
+            repo: 'https://github.com/An-Be/Moviefy',
+            deployed: false,
+            live: ''
+
+        },
+        {
+            name: 'Random Number Generator',
+            description: 'A number generator built with Vanilla JS, HTML, and CSS',
+            repo: 'https://github.com/An-Be/random-number-generator',
+            deployed: true,
+            live: 'https://random-number-generator-kohl.vercel.app/'
+
+        },
+        {
+            name: 'Chattr',
+            description: 'This is a work in progress, it is a full-stack chat app application using React, TailwindCSS, Daisy UI, Express, Socket.io, and Postgresql. I am working on it with Dakotah DuCharme who is in charge of the backend.',
+            repo: 'https://github.com/Multiverse-Workshop/Chattr',
+            deployed: false,
+            live: ''
+
+        },
+    ]
+
+    return (
+        <section className="projects">
+            <h2>Projects</h2>
+            {projects.map((project, index) => 
+                <div key={index}>
+                    <h1>{project.name}</h1>
+                    <p>{project.description}</p>
+                    <a href={project.repo}>REPO</a>
+                    {project.live? <a href={project.deployed}></a> : <div></div>}
+                </div>
+            )}
+        </section>
+    )
 }
 export default Projects;
